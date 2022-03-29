98cb9f8dd27692f9fe409218dbf9407abc43172d
Benoit Penelle <benoit@penelle.net>
2022-03-29 18:05:49 +0200

    mise en place des migration pas encore terminé

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'userType',
     ];
 
     /**
@@ -41,4 +42,17 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
+
+    public function reservations()
+    {
+        return $this->hasMany(reservation::class);
+    }
+    public function bills()
+    {
+        return $this->hasMany(Bill::class);
+    }
 }
---
+++ database/migrations/2022_03_29_153419_create_order_detail_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_detail', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('produit_id');
+            $table->foreignId('order_id');
+            
+            $table->foreign('produit_id')->references('id')->on('produits')
+                    ->onDelete('restrict')->onUpdate('cascade');
+            $table->foreign('order_id')->references('id')->on('orders')
+                    ->onDelete('restrict')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_detail');
+    }
+};
